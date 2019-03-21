97fd442fde2c544501819915e572fc51eae3778a
Taylor Buckner <tbuckner@theappraisallane.com>
2019-03-21 08:45:37 -0500

    Added some simple methods. Temporarily using a flat architecture.

---
+++ py_lazy_vin/helpers.py
@@ -0,0 +1,13 @@
+from random import randint
+
+class Helpers():
+
+    @staticmethod
+    def get_random_number_of_length(length):
+        range_start = 10**(length - 1)
+        range_end = (10**length) - 1
+        return randint(range_start, range_end)
+
+    @staticmethod
+    def get_number_as_string(number):
+        return str(number)
--- py_lazy_vin/py_lazy_vin.py
+++ py_lazy_vin/py_lazy_vin.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from random import choice
+from helpers import Helpers
+# from .hard_coded_vins import HARD_CODED_VINS
+
+class LazyVin:
+    _vin_list = []
+    _unique_identifier_length = 6
+
+    def __init__(self):
+        # self._vin_list = HARD_CODED_VINS
+        self._vin_list = ['JM1GJ1W50E1103389']
+
+
+    def get_random_dirty_vin(self):
+        random_vin = choice(self._vin_list)
+        return random_vin
+
+    def get_random_clean_vin(self):
+        dirty_vin = self.get_random_dirty_vin()
+        # dirty_vin = ''
+        clean_vin = dirty_vin[0:(len(dirty_vin) - self._unique_identifier_length)]
+        random_unique_id = Helpers.get_random_number_of_length(self._unique_identifier_length)
+        clean_vin += Helpers.get_number_as_string(random_unique_id)
+        print(dirty_vin, clean_vin)
+
+
+lazy = LazyVin()
+lazy.get_random_clean_vin()
